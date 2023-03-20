eaa05dce0dabc9acc3ad4dad79c8fc5c6755f42a
Bogdana <kuzminabogdana25@gmail.com>
2023-03-20 10:39:50 +0200

    init

--- src/js/app.js
+++ src/js/app.js
@@ -6,3 +6,67 @@ flsFunctions.isWebp();
 import Swiper, { Navigation, Pagination } from 'swiper';
 
 const swiper = new Swiper();
+
+
+// TABS
+// -------------------
+function Tabs() {
+   var bindAll = function () {
+      var menuElements = document.querySelectorAll('[data-tab]');
+      for (var i = 0; i < menuElements.length; i++) {
+         menuElements[i].addEventListener('click', change, false);
+      }
+   }
+
+   var clear = function () {
+      var menuElements = document.querySelectorAll('[data-tab]');
+      for (var i = 0; i < menuElements.length; i++) {
+         menuElements[i].classList.remove('active');
+         var id = menuElements[i].getAttribute('data-tab');
+         document.getElementById(id).classList.remove('active');
+      }
+   }
+
+   var change = function (e) {
+      clear();
+      e.target.classList.add('active');
+      var id = e.currentTarget.getAttribute('data-tab');
+      document.getElementById(id).classList.add('active');
+   }
+
+   bindAll();
+}
+
+var connectTabs = new Tabs();
+
+// Swiper
+// ------------------
+new Swiper('.image-slider', {
+   slidesPerView: 6,
+   spaceBetween: 30,
+   loop: true,
+   pagination: {
+      // el: ".swiper-points",
+      clickable: true,
+   },
+   // стрелки 
+   navigation: {
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
+   },
+   breakpoints: {
+      0: {
+         slidesPerView: 1,
+         spaceBetween: 20,
+      },
+
+      668: {
+         slidesPerView: 2,
+         spaceBetween: 40,
+      },
+      1024: {
+         slidesPerView: 3,
+         spaceBetween: 30,
+      },
+   },
+});
